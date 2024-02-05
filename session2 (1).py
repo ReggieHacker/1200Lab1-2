ab0c71cb473c55aff936d449d50017b4fc8ebe49
ReggieHacker <157428746+ReggieHacker@users.noreply.github.com>
2024-02-05 09:57:19 -0800

    Add files via upload

---
+++ session2 (1).py
@@ -0,0 +1,54 @@
+'Session 2'
+
+# =============================================================================
+# ##Printing
+# print('Enter your name: ')
+# x = input()
+# 
+# print('Hello, ' + x)
+# =============================================================================
+
+
+# =============================================================================
+# ##Position Equation
+# print('State the acceleration of the object: ')
+# a = float(input())
+# print('State the change in time: ')
+# t = float(input())
+# 
+# s = ((0.5*a)*(t**2))
+# 
+# 
+# print(str(s) + ' meters')
+# =============================================================================
+
+# =============================================================================
+# ##Ball Problem
+# print('Velocity of ball: ')
+# v = float(input())
+# 
+# s = (v**2)/19.6
+# 
+# print(str(s) + ' meters')
+# =============================================================================
+
+
+# =============================================================================
+# ##Box Problem
+# import numpy as np
+# 
+# print('What is the mass of the box: ')
+# m = float(input())
+# 
+# print('What is the coefficient of kinetic friction: ')
+# uk = float(input())
+# 
+# print('What is the angle of the slope in radians: ')
+# rad = float(input())
+# 
+# f = float(np.cos(rad) * m * 9.8 * uk)
+# 
+# print(str(f) + ' N')
+# =============================================================================
+
+
